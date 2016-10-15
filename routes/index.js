3acde49482b163aa2548a9a6e02dcbcffac5ecef
Angelo Zamudio <avaz1301@gmail.com>
2016-10-15 16:43:01 -0400

    Fix result page to route to details page

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CUNYStack' });
+    res.render('index', {
+        title: 'CUNYStack'
+    });
 });
 
-router.post("/results", function (req, res) {
+router.post('/results', function(req, res) {
     res.render('results', {
-        school:req.body.school,
-        subject:req.body.subject,
-        courseID:req.body.courseID
-    })
+        school: req.body.school,
+        subject: req.body.subject,
+        courseID: req.body.courseID
+    });
 });
-
-
-
 
 module.exports = router;
