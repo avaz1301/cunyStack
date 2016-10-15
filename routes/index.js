49839a331ac15da18718f46c73bdb1d424dc0dac
Angelo Zamudio <avaz1301@gmail.com>
2016-10-15 02:39:52 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CUNYStack' });
+});
+
+router.get('/results', function(req,res,next){
+    res.render('results', {});
+});
+
+module.exports = router;
